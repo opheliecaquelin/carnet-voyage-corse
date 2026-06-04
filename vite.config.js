9d419b9ffbd72c465a55452adfbc0898041e4ba2
Ophélie Caquelin <opheliecaquelin@Kiato.local>
2026-06-04 14:35:46 +0200

    Ajout PWA

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,38 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite"
+import react from "@vitejs/plugin-react"
+import { VitePWA } from "vite-plugin-pwa"
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+
+    VitePWA({
+      registerType: "autoUpdate",
+
+      manifest: {
+        name: "Carnet Voyage Corse 2026",
+        short_name: "Carnet Voyage",
+        description:
+          "Carnet de voyage Corse 2026",
+
+        theme_color: "#2563eb",
+        background_color: "#ffffff",
+
+        display: "standalone",
+
+        icons: [
+          {
+            src: "/icon-192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "/icon-512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
+  ],
 })
